db1ee3b38ef25e1e6d1014f807bbfe256324949b
Myles Bartlett <m.bartlett@sussex.ac.uk>
2021-07-29 21:50:23 +0100

    Use palbolts dataset/datamodule for weather data.

--- ssa/weather/data/dataset.py
+++ ssa/weather/data/dataset.py
@@ -1,41 +1,118 @@
+from __future__ import annotations
+from enum import Enum, auto
+import logging
+import os
 from pathlib import Path
-from typing import NamedTuple
+from typing import ClassVar, Union
+from urllib.request import urlopen
 
+from bolts.data.datasets.base import PBDataset
+from kit import parsable
+from kit.misc import str_to_enum
 import pandas as pd
+import requests
 import torch
-from torch import Tensor
-from torch.utils.data import Dataset
-from torch.utils.data.dataset import T_co
+from tqdm import tqdm
+
+__all__ = ["DataSplit", "WeatherDataset"]
 
 
-class BinaryTuple(NamedTuple):
-    x: Tensor
-    y: Tensor
+def download_from_url(
+    url: str,
+    *,
+    dst: str | Path,
+    logger: logging.Logger | None = None,
+) -> None:
+    """Download from a url."""
+    logger = logging.getLogger(__name__) if logger is None else logger
+
+    if isinstance(dst, str):
+        dst = Path(dst)
+
+    if not dst.exists():
+        logger.info(f"Downloading file {dst.name} from address '{url}'.")
+
+        file_size = int(urlopen(url).info().get("Content-Length", -1))
+        first_byte = os.path.getsize(dst) if dst.exists() else 0
+
+        if first_byte < file_size:
+            header = {"Range": "bytes=%s-%s" % (first_byte, file_size)}
+            pbar = tqdm(
+                total=file_size,
+                initial=first_byte,
+                unit="B",
+                unit_scale=True,
+                desc=url.split("/")[-1],
+            )
+            req = requests.get(url, headers=header, stream=True)
+            with (open(dst, "ab")) as f:
+                for chunk in req.iter_content(chunk_size=1024):
+                    if chunk:
+                        f.write(chunk)
+                        pbar.update(1024)
+            pbar.close()
+        else:
+            logger.info(f"File '{dst.name}' already downloaded.")
+    if dst.suffix == ".tar":
+        if dst.with_suffix("").exists():
+            logger.info(f"File '{dst.name}' already extracted.")
+        else:
+            import tarfile
+
+            logger.info(
+                f"Extracting '{dst.resolve()}' to {dst.parent.resolve()}; this could take a while."
+            )
+            with tarfile.TarFile(dst, "r") as fhandle:
+                fhandle.extractall(dst.parent)
 
 
-class WeatherDatasetTest(Dataset):
-    def __init__(self, data_dir: Path):
-        df_dev_in = pd.read_csv(data_dir / "weather" / "dev_in.csv")
-        df_dev_out = pd.read_csv(data_dir / "weather" / "dev_out.csv")
-        df_test = pd.concat([df_dev_in, df_dev_out])
-
-        x = df_test.iloc[:, 6:]
-        y = df_test['fact_temperature']
-        self.x = torch.tensor(x.values)
-        self.y = torch.tensor(y.values)
-
-    def __getitem__(self, index) -> BinaryTuple:
-        return BinaryTuple(x=self.x[index], y=self.y[index])
+class DataSplit(Enum):
+    train = auto()
+    dev = auto()
+    eval = auto()
 
 
-class WeatherDataset(Dataset):
-    def __init__(self, data_dir: Path):
-        df_train = pd.read_csv(data_dir / "weather" / "train.csv")
+class WeatherDataset(PBDataset):
 
-        x = df_train.iloc[:, 6:]
-        y = df_train['fact_temperature']
-        self.x = torch.tensor(x.values)
-        self.y = torch.tensor(y.values)
+    _URL: ClassVar[
+        str
+    ] = "https://storage.yandexcloud.net/yandex-research/shifts/weather/canonical-trn-dev-data.tar"
+    _BASE_FOLDER: ClassVar[str] = "weather"
 
-    def __getitem__(self, index) -> T_co:
-        return BinaryTuple(x=self.x[index], y=self.y[index])
+    @parsable
+    def __init__(
+        self, root: Union[Path, str], split: Union[DataSplit, str], download: bool = True
+    ) -> None:
+        if isinstance(root, str):
+            root = Path(root)
+        self._base_dir = root / self._BASE_FOLDER
+        self._data_dir = self._base_dir / "data"
+        self.download = download
+
+        if self.download:
+            self._base_dir.mkdir(parents=True, exist_ok=True)
+            download_from_url(
+                url=self._URL, dst=self._base_dir / "canonical_trn_dev_data.tar", logger=self.logger
+            )
+
+        elif not self._check_unzipped():
+            raise RuntimeError(
+                f"Data not found at location {self._data_dir.resolve()}. " "Have you downloaded it?"
+            )
+
+        if isinstance(split, str):
+            str_to_enum(str_=split, enum=DataSplit)
+        if split is DataSplit.train:
+            df = pd.read_csv(self._data_dir / "train.csv")
+        else:
+            df_dev_in = pd.read_csv(self._data_dir / "dev_in.csv")
+            df_dev_out = pd.read_csv(self._data_dir / "dev_out.csv")
+            df = pd.concat([df_dev_in, df_dev_out])
+
+        x = torch.as_tensor(df.iloc[:, 6:].to_numpy(), dtype=torch.float32)
+        y = None if split is DataSplit.eval else torch.as_tensor(df['fact_temperature'].to_numpy())
+
+        super().__init__(x=x, y=y)
+
+    def _check_unzipped(self) -> bool:
+        return self._data_dir.exists()
